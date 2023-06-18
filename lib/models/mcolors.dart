bc5bee9869176539851e58dbe134a01f6cd41042
Egemen <Egemen@ZENDOS>
2023-06-18 23:16:50 +0300

    main

--- lib/models/mcolors.dart
+++ lib/models/mcolors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/painting.dart' show Color;
 
 class Mcolors {
+  /// Uygulamamın standart renk kodlarını tutan class
   static const Color background = Color(0xFF0A0A19);
-  static const Color cyan = Color(0xFF87F5F5);
+  static const Color cyan = Color.fromARGB(255, 0, 255, 255);
 }
